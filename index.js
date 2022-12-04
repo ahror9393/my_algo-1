50a004a422579e76cfe758ed4192f7e965fe0208
ahror9393 <ahrordjumabaev@gmail.com>
2022-12-04 20:51:13 +0500

    nmnmn

---
+++ index.js
@@ -0,0 +1,28 @@
+// let ab = prompt("ismingizni kiriting")
+// function MyName(name){
+// alert("Mening ismim " + ab)
+//     console.log("Mening ismim " + ab);
+// }
+// MyName()
+
+// function getMyinfo(name,age,job){
+//     return 'Mening ismim ${name}, yoshim${age}, kasbim${job}';
+// }
+// console.log(getMyinfo("Sanjar"," 24 da"," Dasturchi"));
+// getMyinfo()
+
+function qoshish(a,b){
+    return `3+2 = ${a+b}`
+}
+console.log(qoshish(3,2));
+
+function qoshish1(c,d){
+    return `-3+ -6 = ${-c+ -d}`
+}
+console.log(qoshish1(3,6));
+
+function qoshish2(n,m){
+    return `7+3 = ${n+m}`
+}
+console.log(qoshish2(7,3));
+
